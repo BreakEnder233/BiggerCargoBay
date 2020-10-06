4a1115e1e423095686ea9b9ba1d20556d4aafb83
Break_Ender233 <Break_Ender233@163.com>
2020-10-06 20:47:42 +0800

    First Version

---
+++ BiggerCargoBay/BiggerCargoBayConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using PeterHan.PLib;
+using PeterHan.PLib.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BiggerCargoBay
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class BiggerCargoBayConfiguration : POptions.SingletonOptions<BiggerCargoBayConfiguration>
+    {
+        [Option("CargoBayCapacity", "How many kgs the solid resource could a cargo bay bring.")]
+        [Limit(1000, 100000)]
+        [JsonProperty]
+        public int CargoBayCapacity { get; set; }
+
+        [Option("LiquidCargoBayCapacity", "How many kgs the solid resource could a cargo bay bring.")]
+        [Limit(1000, 100000)]
+        [JsonProperty]
+        public int LiquidCargoBayCapacity { get; set; }
+
+        [Option("GasCargoBayCapacity", "How many kgs the solid resource could a cargo bay bring.")]
+        [Limit(1000, 100000)]
+        [JsonProperty]
+        public int GasCargoBayCapacity { get; set; }
+
+
+        public BiggerCargoBayConfiguration()
+        {
+            CargoBayCapacity = 10000;
+            LiquidCargoBayCapacity = 5000;
+            GasCargoBayCapacity = 2000;
+        }
+    }
+}
---
+++ BiggerCargoBay/Patches.cs
@@ -0,0 +1,80 @@
+﻿using Harmony;
+using PeterHan.PLib;
+using PeterHan.PLib.Options;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace BiggerCargoBay
+{
+    public class Patches
+    {
+        public static class Mod_OnLoad
+        {
+            public static string ModPath;
+            public static string ModDirectory;
+            public static void OnLoad()
+            {
+                ModPath = Assembly.GetExecutingAssembly().Location;
+                Debug.Log("Initialling LightyMod At: " + ModPath);
+                ModDirectory = new FileInfo(ModPath).DirectoryName;
+                if (!File.Exists(Path.Combine(ModDirectory, "config.json")))
+                {
+                    Debug.Log("Configuration file not exists.");
+                    POptions.WriteSettings<BiggerCargoBayConfiguration>(new BiggerCargoBayConfiguration());
+                    Debug.Log("Creating default configuration.");
+                }
+                PUtil.InitLibrary();
+                POptions.RegisterOptions(typeof(BiggerCargoBayConfiguration));
+            }
+        }
+
+        [HarmonyPatch(typeof(CargoBayConfig))]
+        [HarmonyPatch("DoPostConfigureComplete")]
+        public class CargoBayConfigPatch
+        {
+            public static void Prefix()
+            {
+                //
+            }
+
+            public static void Postfix(ref GameObject go)
+            {
+                CargoBay cargoBay = go.AddOrGet<CargoBay>();
+                cargoBay.storage.capacityKg = (float)BiggerCargoBayConfiguration.Instance.CargoBayCapacity;
+            }
+        }
+
+        [HarmonyPatch(typeof(LiquidCargoBayConfig))]
+        [HarmonyPatch("DoPostConfigureComplete")]
+        public class LiquidCargoBayConfigPatch
+        {
+            public static void Prefix()
+            {
+                //
+            }
+
+            public static void Postfix(ref GameObject go)
+            {
+                CargoBay cargoBay = go.AddOrGet<CargoBay>();
+                cargoBay.storage.capacityKg = (float)BiggerCargoBayConfiguration.Instance.LiquidCargoBayCapacity;
+            }
+        }
+
+        [HarmonyPatch(typeof(GasCargoBayConfig))]
+        [HarmonyPatch("DoPostConfigureComplete")]
+        public class GasCargoBayConfigPatch
+        {
+            public static void Prefix()
+            {
+                //
+            }
+
+            public static void Postfix(ref GameObject go)
+            {
+                CargoBay cargoBay = go.AddOrGet<CargoBay>();
+                cargoBay.storage.capacityKg = (float)BiggerCargoBayConfiguration.Instance.GasCargoBayCapacity;
+            }
+        }
+    }
+}
