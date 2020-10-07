26161f44471807434e51e3d126b7bb298e891c62
Break_Ender233 <Break_Ender233@163.com>
2020-10-07 12:06:30 +0800

    Add Always Reset Mass Option

--- BiggerCargoBay/Patches.cs
+++ BiggerCargoBay/Patches.cs
@@ -1,6 +1,8 @@
-﻿using Harmony;
+﻿using Database;
+using Harmony;
 using PeterHan.PLib;
 using PeterHan.PLib.Options;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using UnityEngine;
@@ -76,5 +78,66 @@ namespace BiggerCargoBay
                 cargoBay.storage.capacityKg = (float)BiggerCargoBayConfiguration.Instance.GasCargoBayCapacity;
             }
         }
+
+        [HarmonyPatch(typeof(SpaceDestination))]
+        [HarmonyPatch("UpdateRemainingResources")]
+        public class SpaceDestinationMassPatch
+        {
+            public static bool Prefix(CargoBay bay)
+            {
+                return !BiggerCargoBayConfiguration.Instance.ResetPlanetMass;
+            }
+
+            public static void Postfix(SpaceDestination __instance, CargoBay bay)
+            {
+                if (!BiggerCargoBayConfiguration.Instance.ResetPlanetMass) return;
+                var traverse = Traverse.Create(__instance);
+                SpaceDestinationType destinationType = __instance.GetDestinationType();
+                traverse.SetField("availableMass", destinationType.maxiumMass - __instance.CurrentMass);
+            }
+        }
+
+        /*
+        [HarmonyPatch(typeof(SpaceDestination))]
+        [HarmonyPatch("GetMissionResourceResult")]
+        public class SpaceDestinationResourcePatch
+        {
+            public static bool Prefix(float totalCargoSpace,
+                bool solids = true,
+                bool liquids = true,
+                bool gasses = true)
+            {
+                return !BiggerCargoBayConfiguration.Instance.DontChangeRemainMass;
+            }
+
+            public static void Postfix(
+                SpaceDestination __instance,
+                ref Dictionary<SimHashes, float> __result,
+                float totalCargoSpace,
+                bool solids = true,
+                bool liquids = true,
+                bool gasses = true)
+            {
+                if (!BiggerCargoBayConfiguration.Instance.DontChangeRemainMass) return;
+                Dictionary<SimHashes, float> dictionary = new Dictionary<SimHashes, float>();
+                float num1 = 0.0f;
+                foreach (KeyValuePair<SimHashes, float> recoverableElement in __instance.recoverableElements)
+                {
+                    if (ElementLoader.FindElementByHash(recoverableElement.Key).IsSolid & solids || ElementLoader.FindElementByHash(recoverableElement.Key).IsLiquid & liquids || ElementLoader.FindElementByHash(recoverableElement.Key).IsGas & gasses)
+                        num1 += __instance.GetResourceValue(recoverableElement.Key, recoverableElement.Value);
+                }
+                float num2 = totalCargoSpace;
+                foreach (KeyValuePair<SimHashes, float> recoverableElement in __instance.recoverableElements)
+                {
+                    if (ElementLoader.FindElementByHash(recoverableElement.Key).IsSolid & solids || ElementLoader.FindElementByHash(recoverableElement.Key).IsLiquid & liquids || ElementLoader.FindElementByHash(recoverableElement.Key).IsGas & gasses)
+                    {
+                        float num3 = num2 * (__instance.GetResourceValue(recoverableElement.Key, recoverableElement.Value) / num1);
+                        dictionary.Add(recoverableElement.Key, 1000f);
+                    }
+                }
+                __result = dictionary;
+            }
+        }
+        */
     }
 }
